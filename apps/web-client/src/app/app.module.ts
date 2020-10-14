cd58a1d71cf5c9d60774fc1e75432e136e5a41f8
Daniel Marin <danielarturomt@gmail.com>
2020-10-14 14:05:00 -0300

    feat: create the backbones of the contacts widget

--- apps/web-client/src/app/app.module.ts
+++ apps/web-client/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { ContactsModule } from './contacts/contacts.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ContactsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
