71134b1d4a05f3d1dfb22534ac28efe65b1e152f
Daniel Marin <danielarturomt@gmail.com>
2020-10-21 17:48:59 -0300

    feat: create message via API on send message

--- apps/comm/src/app/app.gateway.ts
+++ apps/comm/src/app/app.gateway.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { HttpService, Logger } from '@nestjs/common';
-import { IUser, ActionTypes, IChat } from '@chat-app/api-interface';
+import { IUser, ActionTypes, IChat, IMessage } from '@chat-app/api-interface';
 
 @WebSocketGateway()
 export class AppGateway {
@@ -78,5 +78,11 @@ export class AppGateway {
     @MessageBody() payload: { authorId: string; chatId: string; body: string }
   ) {
     this.logger.log(`Send Message to chat ${payload.chatId}`);
+    this.httpService
+      .post<IMessage>(
+        `http://localhost:3333/api/chats/${payload.chatId}/messages`,
+        { authorId: payload.authorId, body: payload.body }
+      )
+      .subscribe();
   }
 }
