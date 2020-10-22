09f838f87c365fb78d83aeb1a849530e0b98fc02
Daniel Marin <danielarturomt@gmail.com>
2020-10-22 13:41:22 -0300

    task 2 completed

--- apps/web-client/src/app/app.component.ts
+++ apps/web-client/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'wc-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private socket: Socket) {
+    this.socket.connect();
+  }
+}
